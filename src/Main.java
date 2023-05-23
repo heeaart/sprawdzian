3dffb439f9419595dbf1ecfe24d0f388b75dc373
szymon.sz <szymon366@vp.pl>
2023-05-23 10:29:02 +0200

    kk

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
-/**
- <Opis klasy>
- <pole> : <typ>, <opis pola>
- @author: Ali hatbi
- **/
 
 
 public class Main {
     public static void main(String[] args) {
 
+        Ksiazka ksiazka1 = new Ksiazka("test","test",1920);
+        Ksiazka ksiazka2 = new Ksiazka("50 twarzy mateusza m","maniek",1420);
+        Ksiazka ksiazka3 = new Ksiazka("Fortnite","League of Legends",32);
 
     }
 }
